b986aa094546e653dee30dd84b411b0a9e03aba1
Pavel <Test@email.ru>
2023-05-18 09:05:40 +0400

    crash test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -17,7 +17,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello World"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
